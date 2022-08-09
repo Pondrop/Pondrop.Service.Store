0bc6fe33c31d2caa7ce39e3d7e2320def305c18b
Daniel Gatto <d.mario.gatto@gmail.com>
2022-08-09 21:08:29 +0930

    added more unit tests

---
+++ tests/Pondrop.Service.Store.Application.Tests/Commands/StoreType/CreateStoreType/CreateStoreTypeCommandHandlerTests.cs
@@ -0,0 +1,186 @@
+using AutoMapper;
+using FluentValidation;
+using FluentValidation.Results;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Moq;
+using Pondrop.Service.Store.Application.Commands;
+using Pondrop.Service.Store.Application.Interfaces;
+using Pondrop.Service.Store.Application.Interfaces.Services;
+using Pondrop.Service.Store.Application.Models;
+using Pondrop.Service.Store.Domain.Events;
+using Pondrop.Service.Store.Domain.Models;
+using Pondrop.Service.Store.Tests.Faker;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Pondrop.Service.Store.Application.Tests.Commands.StoreType.CreateStoreType;
+
+public class CreateStoreTypeCommandHandlerTests
+{
+    private readonly Mock<IOptions<StoreTypeUpdateConfiguration>> _StoreTypeUpdateConfigMock;
+    private readonly Mock<IEventRepository> _eventRepositoryMock;
+    private readonly Mock<IDaprService> _daprServiceMock;
+    private readonly Mock<IUserService> _userServiceMock;
+    private readonly Mock<IMapper> _mapperMock;
+    private readonly Mock<IValidator<CreateStoreTypeCommand>> _validatorMock;
+    private readonly Mock<ILogger<CreateStoreTypeCommandHandler>> _loggerMock;
+    
+    public CreateStoreTypeCommandHandlerTests()
+    {
+        _StoreTypeUpdateConfigMock = new Mock<IOptions<StoreTypeUpdateConfiguration>>();
+        _eventRepositoryMock = new Mock<IEventRepository>();
+        _daprServiceMock = new Mock<IDaprService>();
+        _userServiceMock = new Mock<IUserService>();
+        _mapperMock = new Mock<IMapper>();
+        _validatorMock = new Mock<IValidator<CreateStoreTypeCommand>>();
+        _loggerMock = new Mock<ILogger<CreateStoreTypeCommandHandler>>();
+
+        _StoreTypeUpdateConfigMock
+            .Setup(x => x.Value)
+            .Returns(new StoreTypeUpdateConfiguration());
+        _userServiceMock
+            .Setup(x => x.CurrentUserName())
+            .Returns("test/user");
+    }
+    
+    [Fact]
+    public async void CreateStoreTypeCommand_ShouldSucceed()
+    {
+        // arrange
+        var cmd = StoreTypeFaker.GetCreateStoreTypeCommand();
+        var item = StoreTypeFaker.GetStoreTypeRecord(cmd);
+        _validatorMock
+            .Setup(x => x.Validate(cmd))
+            .Returns(new ValidationResult());
+        _eventRepositoryMock
+            .Setup(x => x.AppendEventsAsync(It.IsAny<string>(), It.IsAny<long>(), It.IsAny<IEnumerable<IEvent>>()))
+            .Returns(Task.FromResult(true));
+        _mapperMock
+            .Setup(x => x.Map<StoreTypeRecord>(It.IsAny<StoreTypeEntity>()))
+            .Returns(item);
+        var handler = GetCommandHandler();
+        
+        // act
+        var result = await handler.Handle(cmd, CancellationToken.None);
+        
+        // assert
+        Assert.True(result.IsSuccess);
+        Assert.Equal(item, result.Value);
+        _validatorMock.Verify(
+            x => x.Validate(cmd),
+            Times.Once());
+        _eventRepositoryMock.Verify(
+            x => x.AppendEventsAsync(It.IsAny<string>(), It.IsAny<long>(), It.IsAny<IEnumerable<IEvent>>()),
+            Times.Once());
+        _mapperMock.Verify(
+            x => x.Map<StoreTypeRecord>(It.IsAny<StoreTypeEntity>()),
+            Times.Once);
+    }
+    
+    [Fact]
+    public async void CreateStoreTypeCommand_WhenInvalid_ShouldFail()
+    {
+        // arrange
+        var cmd = StoreTypeFaker.GetCreateStoreTypeCommand();
+        var item = StoreTypeFaker.GetStoreTypeRecord(cmd);
+        _validatorMock
+            .Setup(x => x.Validate(cmd))
+            .Returns(new ValidationResult(new [] { new ValidationFailure() }));
+        var handler = GetCommandHandler();
+        
+        // act
+        var result = await handler.Handle(cmd, CancellationToken.None);
+        
+        // assert
+        Assert.False(result.IsSuccess);
+        _validatorMock.Verify(
+            x => x.Validate(cmd),
+            Times.Once());
+        _eventRepositoryMock.Verify(
+            x => x.AppendEventsAsync(It.IsAny<string>(), It.IsAny<long>(), It.IsAny<IEnumerable<IEvent>>()),
+            Times.Never());
+        _mapperMock.Verify(
+            x => x.Map<StoreTypeRecord>(It.IsAny<StoreTypeEntity>()),
+            Times.Never);
+    }
+
+    [Fact]
+    public async void CreateStoreTypeCommand_WhenAppendEventsFail_ShouldFail()
+    {
+        // arrange
+        var cmd = StoreTypeFaker.GetCreateStoreTypeCommand();
+        var item = StoreTypeFaker.GetStoreTypeRecord(cmd);
+        _validatorMock
+            .Setup(x => x.Validate(cmd))
+            .Returns(new ValidationResult());
+        _eventRepositoryMock
+            .Setup(x => x.AppendEventsAsync(It.IsAny<string>(), It.IsAny<long>(), It.IsAny<IEnumerable<IEvent>>()))
+            .Returns(Task.FromResult(false));
+        _mapperMock
+            .Setup(x => x.Map<StoreTypeRecord>(It.IsAny<StoreTypeEntity>()))
+            .Returns(item);
+        var handler = GetCommandHandler();
+        
+        // act
+        var result = await handler.Handle(cmd, CancellationToken.None);
+        
+        // assert
+        Assert.False(result.IsSuccess);
+        _validatorMock.Verify(
+            x => x.Validate(cmd),
+            Times.Once());
+        _eventRepositoryMock.Verify(
+            x => x.AppendEventsAsync(It.IsAny<string>(), It.IsAny<long>(), It.IsAny<IEnumerable<IEvent>>()),
+            Times.Once());
+        _mapperMock.Verify(
+            x => x.Map<StoreTypeRecord>(It.IsAny<StoreTypeEntity>()),
+            Times.Never);
+    }
+    
+    [Fact]
+    public async void CreateStoreTypeCommand_WhenException_ShouldFail()
+    {
+        // arrange
+        var cmd = StoreTypeFaker.GetCreateStoreTypeCommand();
+        var item = StoreTypeFaker.GetStoreTypeRecord(cmd);
+        _validatorMock
+            .Setup(x => x.Validate(cmd))
+            .Returns(new ValidationResult());
+        _eventRepositoryMock
+            .Setup(x => x.AppendEventsAsync(It.IsAny<string>(), It.IsAny<long>(), It.IsAny<IEnumerable<IEvent>>()))
+            .Throws(new Exception());
+        _mapperMock
+            .Setup(x => x.Map<StoreTypeRecord>(It.IsAny<StoreTypeEntity>()))
+            .Returns(item);
+        var handler = GetCommandHandler();
+        
+        // act
+        var result = await handler.Handle(cmd, CancellationToken.None);
+        
+        // assert
+        Assert.False(result.IsSuccess);
+        _validatorMock.Verify(
+            x => x.Validate(cmd),
+            Times.Once());
+        _eventRepositoryMock.Verify(
+            x => x.AppendEventsAsync(It.IsAny<string>(), It.IsAny<long>(), It.IsAny<IEnumerable<IEvent>>()),
+            Times.Once());
+        _mapperMock.Verify(
+            x => x.Map<StoreTypeRecord>(It.IsAny<StoreTypeEntity>()),
+            Times.Never);
+    }
+    
+    private CreateStoreTypeCommandHandler GetCommandHandler() =>
+        new CreateStoreTypeCommandHandler(
+            _StoreTypeUpdateConfigMock.Object,
+            _eventRepositoryMock.Object,
+            _daprServiceMock.Object,
+            _userServiceMock.Object,
+            _mapperMock.Object,
+            _validatorMock.Object,
+            _loggerMock.Object);
+}
---
+++ tests/Pondrop.Service.Store.Application.Tests/Queries/Retailer/GetRetailerById/GetRetailerByIdHandlerTests.cs
@@ -0,0 +1,176 @@
+using AutoMapper;
+using FluentValidation;
+using FluentValidation.Results;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Pondrop.Service.Store.Application.Commands;
+using Pondrop.Service.Store.Application.Interfaces;
+using Pondrop.Service.Store.Application.Queries;
+using Pondrop.Service.Store.Domain.Events;
+using Pondrop.Service.Store.Domain.Models;
+using Pondrop.Service.Store.Tests.Faker;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Pondrop.Service.Store.Application.Tests.Commands.Retailer.CreateRetailer;
+
+public class GetRetailerByIdHandlerTests
+{
+    private readonly Mock<IMaterializedViewRepository<RetailerEntity>> _viewRepositoryMock;
+    private readonly Mock<IMapper> _mapperMock;
+    private readonly Mock<IValidator<GetRetailerByIdQuery>> _validatorMock;
+    private readonly Mock<ILogger<GetRetailerByIdQueryHandler>> _loggerMock;
+    
+    public GetRetailerByIdHandlerTests()
+    {
+        _viewRepositoryMock = new Mock<IMaterializedViewRepository<RetailerEntity>>();
+        _mapperMock = new Mock<IMapper>();
+        _validatorMock = new Mock<IValidator<GetRetailerByIdQuery>>();
+        _loggerMock = new Mock<ILogger<GetRetailerByIdQueryHandler>>();
+    }
+    
+    [Fact]
+    public async void GetRetailerByIdQuery_ShouldSucceed()
+    {
+        // arrange
+        var query = new GetRetailerByIdQuery() { Id = Guid.NewGuid() };
+        var item = RetailerFaker.GetRetailerRecords(1).Single();
+        _validatorMock
+            .Setup(x => x.Validate(query))
+            .Returns(new ValidationResult());
+        _viewRepositoryMock
+            .Setup(x => x.GetByIdAsync(query.Id))
+            .Returns(Task.FromResult<RetailerEntity?>(new RetailerEntity()));
+        _mapperMock
+            .Setup(x => x.Map<RetailerRecord>(It.IsAny<RetailerEntity>()))
+            .Returns(item);
+        var handler = GetQueryHandler();
+        
+        // act
+        var result = await handler.Handle(query, CancellationToken.None);
+        
+        // assert
+        Assert.True(result.IsSuccess);
+        Assert.Equal(item, result.Value);
+        _validatorMock.Verify(
+            x => x.Validate(query),
+            Times.Once());
+        _viewRepositoryMock.Verify(
+            x => x.GetByIdAsync(query.Id),
+            Times.Once());
+        _mapperMock.Verify(
+            x => x.Map<RetailerRecord>(It.IsAny<RetailerEntity>()),
+            Times.Once());
+    }
+    
+    [Fact]
+    public async void GetRetailerByIdQuery_WhenInvalid_ShouldFail()
+    {
+        // arrange
+        var query = new GetRetailerByIdQuery() { Id = Guid.NewGuid() };
+        var item = RetailerFaker.GetRetailerRecords(1).Single();
+        _validatorMock
+            .Setup(x => x.Validate(query))
+            .Returns(new ValidationResult(new [] { new ValidationFailure() }));
+        _viewRepositoryMock
+            .Setup(x => x.GetByIdAsync(query.Id))
+            .Returns(Task.FromResult<RetailerEntity?>(new RetailerEntity()));
+        _mapperMock
+            .Setup(x => x.Map<RetailerRecord>(It.IsAny<RetailerEntity>()))
+            .Returns(item);
+        var handler = GetQueryHandler();
+        
+        // act
+        var result = await handler.Handle(query, CancellationToken.None);
+        
+        // assert
+        Assert.False(result.IsSuccess);
+        _validatorMock.Verify(
+            x => x.Validate(query),
+            Times.Once());
+        _viewRepositoryMock.Verify(
+            x => x.GetByIdAsync(query.Id),
+            Times.Never());
+        _mapperMock.Verify(
+            x => x.Map<RetailerRecord>(It.IsAny<RetailerEntity>()),
+            Times.Never());
+    }
+    
+    [Fact]
+    public async void GetRetailerByIdQuery_WhenNotFound_ShouldSucceedWithNull()
+    {
+        // arrange
+        var query = new GetRetailerByIdQuery() { Id = Guid.NewGuid() };
+        var item = RetailerFaker.GetRetailerRecords(1).Single();
+        _validatorMock
+            .Setup(x => x.Validate(query))
+            .Returns(new ValidationResult());
+        _viewRepositoryMock
+            .Setup(x => x.GetByIdAsync(query.Id))
+            .Returns(Task.FromResult<RetailerEntity?>(null));
+        _mapperMock
+            .Setup(x => x.Map<RetailerRecord>(It.IsAny<RetailerEntity>()))
+            .Returns(item);
+        var handler = GetQueryHandler();
+        
+        // act
+        var result = await handler.Handle(query, CancellationToken.None);
+        
+        // assert
+        Assert.True(result.IsSuccess);
+        Assert.Null(result.Value);
+        _validatorMock.Verify(
+            x => x.Validate(query),
+            Times.Once());
+        _viewRepositoryMock.Verify(
+            x => x.GetByIdAsync(query.Id),
+            Times.Once());
+        _mapperMock.Verify(
+            x => x.Map<RetailerRecord>(It.IsAny<RetailerEntity>()),
+            Times.Never());
+    }
+    
+    [Fact]
+    public async void GetRetailerByIdQuery_WhenThrows_ShouldFail()
+    {
+        // arrange
+        var query = new GetRetailerByIdQuery() { Id = Guid.NewGuid() };
+        var item = RetailerFaker.GetRetailerRecords(1).Single();
+        _validatorMock
+            .Setup(x => x.Validate(query))
+            .Returns(new ValidationResult());
+        _viewRepositoryMock
+            .Setup(x => x.GetByIdAsync(query.Id))
+            .Throws(new Exception());
+        _mapperMock
+            .Setup(x => x.Map<RetailerRecord>(It.IsAny<RetailerEntity>()))
+            .Returns(item);
+        var handler = GetQueryHandler();
+        
+        // act
+        var result = await handler.Handle(query, CancellationToken.None);
+        
+        // assert
+        Assert.False(result.IsSuccess);
+        _validatorMock.Verify(
+            x => x.Validate(query),
+            Times.Once());
+        _viewRepositoryMock.Verify(
+            x => x.GetByIdAsync(query.Id),
+            Times.Once());
+        _mapperMock.Verify(
+            x => x.Map<RetailerRecord>(It.IsAny<RetailerEntity>()),
+            Times.Never());
+    }
+    
+    private GetRetailerByIdQueryHandler GetQueryHandler() =>
+        new GetRetailerByIdQueryHandler(
+            _viewRepositoryMock.Object,
+            _mapperMock.Object,
+            _validatorMock.Object,
+            _loggerMock.Object);
+}
