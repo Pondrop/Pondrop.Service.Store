015e0da9051adef3235dc1e7110ee27a9c41519a
Bryan <Bryan@xam.com.au>
2023-01-12 23:44:31 +0000

    Merged PR 450: Removed other validations for add address
    Related work items: #1314

--- src/Pondrop.Service.Store.Application/Commands/Store/AddAddressToStore/AddAddressToStoreCommandHandlerValidator.cs
+++ src/Pondrop.Service.Store.Application/Commands/Store/AddAddressToStore/AddAddressToStoreCommandHandlerValidator.cs
@@ -12,13 +12,13 @@ public class AddAddressToStoreCommandHandlerValidator : AbstractValidator<AddAdd
         _addressService = addressService;
         
         RuleFor(x => x.StoreId).NotEmpty();
-        RuleFor(x => x.ExternalReferenceId).NotEmpty();
-        RuleFor(x => x.AddressLine1).NotEmpty();
-        RuleFor(x => x.Suburb).NotEmpty();
-        RuleFor(x => x.Postcode).Must(x => _addressService.IsValidAustralianPostcode(x));
-        RuleFor(x => x.State).Must(x => _addressService.IsValidAustralianState(x));
-        RuleFor(x => x.Country).Must(x => _addressService.IsValidAustralianCountry(x));
-        RuleFor(x => x.Latitude).Must(x => x != 0);
-        RuleFor(x => x.Longitude).Must(x => x != 0);
+        //RuleFor(x => x.ExternalReferenceId).NotEmpty();
+        //RuleFor(x => x.AddressLine1).NotEmpty();
+        //RuleFor(x => x.Suburb).NotEmpty();
+        //RuleFor(x => x.Postcode).Must(x => _addressService.IsValidAustralianPostcode(x));
+        //RuleFor(x => x.State).Must(x => _addressService.IsValidAustralianState(x));
+        //RuleFor(x => x.Country).Must(x => _addressService.IsValidAustralianCountry(x));
+        //RuleFor(x => x.Latitude).Must(x => x != 0);
+        //RuleFor(x => x.Longitude).Must(x => x != 0);
     }
 }
