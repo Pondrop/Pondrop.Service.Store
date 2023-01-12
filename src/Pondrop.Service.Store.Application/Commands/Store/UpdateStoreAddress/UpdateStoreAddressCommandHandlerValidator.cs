efd9fb43792d1cb8d3e87f8947bdaa2022f88dd8
Bryan <Bryan@xam.com.au>
2023-01-12 23:55:16 +0000

    Merged PR 452: Address for updates optional
    Related work items: #1314

--- src/Pondrop.Service.Store.Application/Commands/Store/UpdateStoreAddress/UpdateStoreAddressCommandHandlerValidator.cs
+++ src/Pondrop.Service.Store.Application/Commands/Store/UpdateStoreAddress/UpdateStoreAddressCommandHandlerValidator.cs
@@ -13,12 +13,12 @@ public class UpdateStoreAddressCommandHandlerValidator : AbstractValidator<Updat
         
         RuleFor(x => x.Id).NotEmpty();
         RuleFor(x => x.StoreId).NotEmpty();
-        RuleFor(x => x.AddressLine1).Must(x => x is null || !string.IsNullOrEmpty(x));
-        RuleFor(x => x.Suburb).Must(x => x is null || !string.IsNullOrEmpty(x));
-        RuleFor(x => x.Postcode).Must(x => x is null || _addressService.IsValidAustralianPostcode(x));
-        RuleFor(x => x.State).Must(x => x is null ||_addressService.IsValidAustralianState(x));
-        RuleFor(x => x.Country).Must(x => x is null ||_addressService.IsValidAustralianCountry(x));
-        RuleFor(x => x.Latitude).Must(x => x is null || x != 0);
-        RuleFor(x => x.Longitude).Must(x => x is null || x != 0);
+        //RuleFor(x => x.AddressLine1).Must(x => x is null || !string.IsNullOrEmpty(x));
+        //RuleFor(x => x.Suburb).Must(x => x is null || !string.IsNullOrEmpty(x));
+        //RuleFor(x => x.Postcode).Must(x => x is null || _addressService.IsValidAustralianPostcode(x));
+        //RuleFor(x => x.State).Must(x => x is null ||_addressService.IsValidAustralianState(x));
+        //RuleFor(x => x.Country).Must(x => x is null ||_addressService.IsValidAustralianCountry(x));
+        //RuleFor(x => x.Latitude).Must(x => x is null || x != 0);
+        //RuleFor(x => x.Longitude).Must(x => x is null || x != 0);
     }
 }
